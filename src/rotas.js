cadda97a6df79aed41ead4809496f6596c9b80a7
Miguel Freire <msfreire52@gmail.com>
2023-12-02 17:22:18 -0300

    Adicionado a rota detalharUsuario

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { listarCategorias } = require('./controladores/categoria')
-const { cadastrarUsuario, login } = require('./controladores/usuario')
+const { cadastrarUsuario, login, detalharUsuario } = require('./controladores/usuario')
 const rotas = express()
 
 rotas.get('/categoria', listarCategorias )
 rotas.post('/usuario', cadastrarUsuario)
 rotas.post('/login', login)
+rotas.get('/usuario', detalharUsuario)
 
 module.exports = rotas
