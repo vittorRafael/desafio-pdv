4652f47e876efa56fd81de7ad1b6f96728ac345f
CertBR1 <mateus.ribeiro3591@gmail.com>
2023-12-03 02:08:19 -0300

    Refatorado intermediarioValidarTokenUsuario

--- src/intermediario/intermediarioValidacaoTokenUsuarioLogin.js
+++ src/intermediario/intermediarioValidacaoTokenUsuarioLogin.js
@@ -1,26 +1,18 @@
-const knex = require('../config/configBancoDeDados');
+const repositorioUsuario = require('../repositorio/repositorioUsuario');
 const jwt = require('jsonwebtoken');
-const senhaHash = require('../seguranca/senha_hash');
+
 
 const intermediarioValidarTokenUsuario = async (req, res, next) => {
     const { authorization } = req.headers;
-
     if (!authorization) {
         return res.status(401).json({ mensagem: "Para acessar este recurso um token de autenticação válido deve ser enviado." })
     }
-    
     try {
-
-        const token = authorization.split(' ')[1]
-
-        const { id } = jwt.verify(token, senhaHash)
-        
-        const usuario = await knex('usuarios').select('*').where('id', id.id)
-
-        req.usuario = usuario[0]
-        
+        const token = authorization.split(' ')[1];
+        const { id } = jwt.verify(token, process.env.JWT_PASS);
+        const usuarioEncontrado = repositorioUsuario.encontrarUsuario(id);
+        req.usuarioEncontrado = usuarioEncontrado[0];
         next()
-        
     } catch (error) {
         if ('jwt expired') {
             return res.status(400).json('Sua sessão terminou, por favor efetue novamente seu login.')
